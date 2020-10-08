03f31e17a2b83e97d6625d7cc9621be05e79a2c4
dada513 <kontakt@dada513.eu>
2020-10-08 13:52:49 +0200

    abandon the webui for now

--- src/web/server.js
+++
@@ -1,8 +0,0 @@
-import * as config from "../../configuration";
-import express from "express";
-import ls from "log-symbols";
-export const app = express();
-
-app.listen(config.web.port, () =>
-  console.log(ls.success, "Web server listening on", config.web.port)
-);
